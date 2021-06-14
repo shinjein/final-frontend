12311087dcf0e628dfac651ace53128b2ad302d3
shinjein <shinjein@hu-berlin.de>
2021-06-14 14:29:16 +0200

    changed a text

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,7 @@ class About extends React.Component {
         <p>often times we go abroad and know nobody in our new destination. Using your own real network, the app will show you mutual contacts per searched city by only
           one degree of separation (so friends of friends).</p>
         <p>To use: simply create an account and search your friends by username. Then search your new destination by city name, and a list of your contacts with connections in the searched city will appear. </p>
+        <p>DEMO ACCOUNT: log in using the username "demo" and password "Dem0123" to see how it works. </p>
         <p className="disclaimer">
         e-contacts is a passion project of Jeein Shin presented as her final project for IronHack Lisbon in May 2021. It is currently still in development! 
         </p>
